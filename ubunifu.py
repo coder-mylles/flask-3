43fcbbc9d980c0fc85db3f60bce72f4b2ae41eef
martin-moringa <martin.mylles@student.moringachool.com>
2021-06-16 09:15:56 +0300

    Adds a post class that defines the post made by users

--- ubunifu.py
+++ ubunifu.py
@@ -2,15 +2,10 @@ from datetime import datetime
 from flask import Flask,render_template,url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-
-
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '450922fba6e46eeb36b12d8a7635aa92836d3dbe'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
-
-
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -18,11 +13,19 @@ class User(db.Model):
     image_file = db.Column(db.String(20), nullable=False, default="default.jpeg")
     password = db.Column(db.String(20), nullable=False)
     posts = db.relationship('Post',backref='author', lazy=True)
-
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100),nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text,nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
 
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 
 
