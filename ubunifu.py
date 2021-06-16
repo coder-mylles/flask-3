6f7c8c0c07006f63f3156ad14846132aab040911
martin-moringa <martin.mylles@student.moringachool.com>
2021-06-16 07:23:08 +0300

    Adds form.py and defines the registration and login forms functionlity

--- ubunifu.py
+++ ubunifu.py
@@ -2,14 +2,16 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = '450922fba6e46eeb36b12d8a7635aa92836d3dbe'
+
 @app.route("/")
-def main():
-    return "Hello ip"
-
-@app.route("/About")
-def about():
-    return "About this"
+def index():
+    title = "Ubunifu;the creatives haven"
+    return render_template("index.html",title=title)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("about.html")
+
