b1ca731a2212088989900e13da7df6376ebac473
martin-moringa <martin.mylles@student.moringachool.com>
2021-06-16 09:00:06 +0300

    Enables users to receieve message to corresponding error

--- ubunifu.py
+++ ubunifu.py
@@ -1,17 +1,29 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = '450922fba6e46eeb36b12d8a7635aa92836d3dbe'
-
-@app.route("/")
-def index():
+     @app.route("/")
+    def index():
     title = "Ubunifu;the creatives haven"
     return render_template("index.html",title=title)
 
 
-@app.route("/register")
-def register():
-    form = RegistrationForm()
-    return render_template("about.html")
+     @app.route("/register", methods=['GET', 'POST'])
+     def register():
+         form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('index'))
+    return render_template("register.html", form = form) #title="Register"
 
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", form = form)  #title="login"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
